1b350a37db64cd1d56d31c271507037ae6188c97
Charles TISSIER <charles@vingtcinq.io>
2016-03-30 13:38:13 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,8 @@
 from mailchimp import MailChimp
 
  
-client = MailChimp('didierserrat', 'dde1a01e091c530def679e42f2390b46-us9')
+client = MailChimp('MAILCHIMP_USER', 'MAILCHIMP_SECRET')
 
 # print client.authorized_app.all()
 
-# print client.authorized_app.get('870970111619')
-
 # print client.automation.all()
-
-# print client.automation.start('870970111619')
-
-print client.growth.all('2822d76480')

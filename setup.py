41b69d21a6f175bd2d12b4fed6b6fedc7544bf32
Stephen Ross II <sross@mediweightloss.com>
2016-08-01 10:04:53 -0400

    Update version from 1.0.26 to 1.1.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except Exception:
 
 setup(
     name='mailchimp3',
-    version='1.0.26',
+    version='1.1.0',
     description='A python client for v3 of MailChimp API',
     long_description=long_description,
     url='https://github.com/charlesthk/python-mailchimp',

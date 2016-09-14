14559b3f0f9e4483e6a328f7a8c777a94142f27f
Stephen Ross II <sross@mediweightloss.com>
2016-09-14 16:54:14 -0400

    Document the helpers functions
    * Add docstrings to everything
    * Add file encoding
    * Remove unused import

--- mailchimp3/helpers.py
+++ mailchimp3/helpers.py
@@ -1,5 +1,8 @@
+# coding=utf-8
+"""
+Helper functions to perform simple tasks for multiple areas of the API
+"""
 import hashlib
-
 
 HTTP_METHOD_ACTION_MATCHING = {
     'get': 'GET',
@@ -12,16 +15,29 @@ HTTP_METHOD_ACTION_MATCHING = {
 
 def get_subscriber_hash(member_email):
     """
-    The MD5 hash of the lowercase version
-    of the list member's email.
-    Uses as member_id
+    The MD5 hash of the lowercase version of the list member's email.
+    Uses as memeber_id
+
+    :param member_email: The member's email address
+    :type member_email: :py:class:`str`
+    :returns: The md5 hash in hex
+    :rtype: :py:class:`str`
     """
     member_email = member_email.lower().encode()
     m = hashlib.md5(member_email)
     return m.hexdigest()
 
 def merge_two_dicts(x, y):
-    '''Given two dicts, merge them into a new dict as a shallow copy.'''
+    """
+    Given two dicts, merge them into a new dict as a shallow copy.
+
+    :param x: The first dictionary
+    :type x: :py:class:`dict`
+    :param y: The second dictionary
+    :type y: :py:class:`dict`
+    :returns: The merged dictionary
+    :rtype: :py:class:`dict`
+    """
     z = x.copy()
     z.update(y)
     return z

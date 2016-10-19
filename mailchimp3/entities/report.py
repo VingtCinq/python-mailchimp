a11455714c0c3dcb3e806e0517e36d0721b52a8c
Stephen Ross II <sross@mediweightloss.com>
2016-10-19 08:41:31 -0400

    Update Report endpoints
    * Add docstrings to everything
    * Add file encodings
    * Make all imports absolute
    * Rename reportunsubscribed.py, and reportactivity.py
    * Rename ReportActivity() and ReportUnsubscribed()
    * Implement Advice, Click-Detail, Domain Performance, EepURL, Location,
      Sent-To, and SubReport reports
    * Implement all missing methods
    * Implement query string parameters where missing with **queryparams

--- mailchimp3/entities/report.py
+++ mailchimp3/entities/report.py
@@ -1,21 +1,81 @@
+# coding=utf-8
+"""
+The Reports API endpoint
+
+Documentation: http://developer.mailchimp.com/documentation/mailchimp/reference/reports/
+Schema: https://api.mailchimp.com/schema/3.0/Reports/Instance.json
+"""
 from __future__ import unicode_literals
-from ..baseapi import BaseApi
+
+from mailchimp3.baseapi import BaseApi
+from mailchimp3.entities.reportabuse import ReportAbuse
+from mailchimp3.entities.reportadvice import ReportAdvice
+from mailchimp3.entities.reportclickdetail import ReportClickDetail
+from mailchimp3.entities.reportdomainperformance import ReportDomainPerformance
+from mailchimp3.entities.reporteepurl import ReportEepURL
+from mailchimp3.entities.reportemailactivity import ReportEmailActivity
+from mailchimp3.entities.reportlocation import ReportLocation
+from mailchimp3.entities.reportsentto import ReportSentTo
+from mailchimp3.entities.reportsubreport import ReportSubReport
+from mailchimp3.entities.reportunsubscribe import ReportUnsubscribe
 
 
 class Report(BaseApi):
-
+    """
+    Manage campaign reports for your MailChimp account. All Reports endpoints
+    are read-only. MailChimp’s campaign and Automation reports analyze clicks,
+    opens, subscribers’ social activity, e-commerce data, and more.
+    """
     def __init__(self, *args, **kwargs):
+        """
+        Initialize the endpoint
+        """
         super(Report, self).__init__(*args, **kwargs)
         self.endpoint = 'reports'
+        self.campaign_id = None
+        self.abuse = ReportAbuse(self)
+        self.advice = ReportAdvice(self)
+        self.clickdetail = ReportClickDetail(self)
+        self.domainperformance = ReportDomainPerformance(self)
+        self.eepurl = ReportEepURL(self)
+        self.emailactivity = ReportEmailActivity(self)
+        self.location = ReportLocation(self)
+        self.sentto = ReportSentTo(self)
+        self.subreport = ReportSubReport(self)
+        self.unsubscribe = ReportUnsubscribe(self)
 
-    def all(self, **queryparams):
-        """
-        Returns a list of reports.
-        """
-        return self._mc_client._get(url=self.endpoint, **queryparams)
 
-    def get(self, report_id):
+    def all(self, get_all=False, **queryparams):
         """
-        Returns a report for a specific campaign.
+        Get campaign reports.
+
+        :param get_all: Should the query get all results
+        :type get_all: :py:class:`bool`
+        :param queryparams: The query string parameters
+        queryparams['fields'] = []
+        queryparams['exclude_fields'] = []
+        queryparams['count'] = integer
+        queryparams['offset'] = integer
+        queryparams['type'] = []
+        queryparams['before_send_time'] = string
+        queryparams['since_send_time'] = string
         """
-        return self._mc_client._get(url=self._build_path(report_id))
+        self.campaign_id = None
+        if get_all:
+            return self._iterate(url=self._build_path(), **queryparams)
+        else:
+            return self._mc_client._get(url=self._build_path(), **queryparams)
+
+
+    def get(self, campaign_id, **queryparams):
+        """
+        Get report details for a specific sent campaign.
+
+        :param campaign_id: The unique id for the campaign.
+        :type campaign_id: :py:class:`str`
+        :param queryparams: The query string parameters
+        queryparams['fields'] = []
+        queryparams['exclude_fields'] = []
+        """
+        self.campaign_id = campaign_id
+        return self._mc_client._get(url=self._build_path(campaign_id), **queryparams)
--- mailchimp3/entities/reportabuse.py
+++ mailchimp3/entities/reportabuse.py
@@ -1,30 +1,55 @@
+# coding=utf-8
+"""
+The Campaign Abuse Report API endpoint
+
+Documentation: http://developer.mailchimp.com/documentation/mailchimp/reference/reports/abuse-reports/
+"""
 from __future__ import unicode_literals
-from ..baseapi import BaseApi
+
+from mailchimp3.baseapi import BaseApi
 
 
 class ReportAbuse(BaseApi):
-
     """
-    Reports Abuse Collection
-        A list of abuse complaints for a specific campaign.
-        http://kb.mailchimp.com/api/resources/reports/abuse/reports-abuse-collection
-    Reports Abuse Instance
-        A specific abuse report associated with a specific campaign.
-        http://kb.mailchimp.com/api/resources/reports/abuse/reports-abuse-instance
+    Get information about campaign abuse complaints.
     """
-
     def __init__(self, *args, **kwargs):
+        """
+        Initialize the endpoint
+        """
         super(ReportAbuse, self).__init__(*args, **kwargs)
         self.endpoint = 'reports'
+        self.campaign_id = None
+        self.report_id = None
+
 
     def all(self, campaign_id, **queryparams):
         """
-        Returns the last 10 abuse reports by date.
-        """
-        return self._mc_client._get(url=self._build_path(campaign_id, 'abuse-report'), **queryparams)
+        Get a list of abuse complaints for a specific campaign.
 
-    def get(self, campaign_id, abuse_id):
+        :param campaign_id: The unique id for the campaign.
+        :type campaign_id: :py:class:`str`
+        :param queryparams: The query string parameters
+        queryparams['fields'] = []
+        queryparams['exclude_fields'] = []
         """
-        Returns an individual abuse report.
+        self.campaign_id = campaign_id
+        self.report_id = None
+        return self._mc_client._get(url=self._build_path(campaign_id, 'abuse-reports'), **queryparams)
+
+
+    def get(self, campaign_id, report_id, **queryparams):
         """
-        return self._mc_client._get(url=self._build_path(campaign_id, 'abuse-report', abuse_id))
+        Get information about a specific abuse report for a campaign.
+
+        :param campaign_id: The unique id for the campaign.
+        :type campaign_id: :py:class:`str`
+        :param report_id: The id for the abuse report.
+        :type report_id: :py:class:`str`
+        :param queryparams: The query string parameters
+        queryparams['fields'] = []
+        queryparams['exclude_fields'] = []
+        """
+        self.campaign_id = campaign_id
+        self.report_id = report_id
+        return self._mc_client._get(url=self._build_path(campaign_id, 'abuse-reports', report_id), **queryparams)

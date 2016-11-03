dbd3dc7519c87da99acd4c024ff2896f0d573c59
stephenross <stephenross@users.noreply.github.com>
2016-11-02 21:29:36 -0400

    Add patch method for list webhook per update log
    Update log from 2016-10-27 contained new patch method for webhooks

--- mailchimp3/entities/listwebhooks.py
+++ mailchimp3/entities/listwebhooks.py
@@ -86,6 +86,20 @@ class ListWebhooks(BaseApi):
         return self._mc_client._get(url=self._build_path(list_id, 'webhooks', webhook_id))
 
 
+    def update(self, list_id, webhook_id, data):
+        """
+        Update the settings for an existing webhook.
+
+        :param list_id: The unique id for the list
+        :type list_id: :py:class:`str`
+        :param webhook_id: The unique id for the webhook
+        :type webhook_id: :py:class:`str`
+        """
+        self.list_id = list_id
+        self.webhook_id = webhook_id
+        return self._mc_client._patch(url=self._build_path(list_id, 'webhooks', webhook_id), data=data)
+
+
     def delete(self, list_id, webhook_id):
         """
         Delete a specific webhook in a list.

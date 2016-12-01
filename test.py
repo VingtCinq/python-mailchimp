bec9580569386201647a2a8e88a652df683efb2f
Jason Rankin <rankin.jason@gmail.com>
2016-12-01 15:27:39 -0500

    Add python-dotenv and set up test.py as test suite
    This allows us to set mailchimp usernamd and api_key as env vars for
    testing. Also makes the tests runnable via setup.py.

--- test.py
+++ test.py
@@ -2,8 +2,20 @@
 """
 Some basic tests to verify that the wrapper is working
 """
+from os import getenv
+
+from dotenv import load_dotenv, find_dotenv
 from mailchimp3 import MailChimp
 
+load_dotenv(find_dotenv())
+
+client = MailChimp(getenv('MAILCHIMP_USERNAME'), getenv('MAILCHIMP_SECRET'))
+
+print(client.lists.all(fields="lists.name,lists.id"))
+
+print(client.authorized_apps.all(get_all=False))
+
+print(client.automations.all(get_all=True))
 
 client = MailChimp('MAILCHIMP_USER', 'MAILCHIMP_SECRET')
 

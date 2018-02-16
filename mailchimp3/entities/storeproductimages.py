fdeb0a4d3cdf40f5112467bb0f374d212542c13b
Stephen Ross II <sross@mediweightloss.com>
2018-02-15 21:42:10 -0500

    Update calls with queryparams instead of kwargs
    This updates all calls that are providing queryparams to use that name instead
    of kwargs to separate them from functions that are not passing the extra
    arguments to the API.

--- mailchimp3/entities/storeproductimages.py
+++ mailchimp3/entities/storeproductimages.py
@@ -79,7 +79,7 @@ class StoreProductImages(BaseApi):
             return self._mc_client._post(url=self._build_path(store_id, 'products', product_id, 'images'), **queryparams)
 
 
-    def get(self, store_id, product_id, image_id, **kwargs):
+    def get(self, store_id, product_id, image_id, **queryparams):
         """
         Get information about a specific product image.
 
@@ -89,16 +89,16 @@ class StoreProductImages(BaseApi):
         :type product_id: :py:class:`str`
         :param image_id: The id for the product image.
         :type image_id: :py:class:`str`
-        :param kwargs: The query string parameters
-        kwargs['fields'] = []
-        kwargs['exclude_fields'] = []
+        :param queryparams: The query string parameters
+        queryparams['fields'] = []
+        queryparams['exclude_fields'] = []
         """
         self.store_id = store_id
         self.product_id = product_id
         self.image_id = image_id
         return self._mc_client._post(
             url=self._build_path(store_id, 'products', product_id, 'images', image_id),
-            **kwargs
+            **queryparams
         )
 
 

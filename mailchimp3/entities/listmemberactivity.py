5e52f2a15f6ec1f08a55e951ef0454272fbfb5b5
Prateek Goel <68638953+mowgliamu@users.noreply.github.com>
2021-03-05 23:44:07 +0100

    Update listmemberactivity.py
    Added activity-feed, which can be called as client.lists.members.activity.feed

--- mailchimp3/entities/listmemberactivity.py
+++ mailchimp3/entities/listmemberactivity.py
@@ -43,3 +43,23 @@ class ListMemberActivity(BaseApi):
         self.list_id = list_id
         self.subscriber_hash = subscriber_hash
         return self._mc_client._get(url=self._build_path(list_id, 'members', subscriber_hash, 'activity'), **queryparams)
+    
+    def feed(self, list_id, subscriber_hash, **queryparams):
+        """
+        Get the last 50 events of a member’s activity on a specific list,
+        including opens, clicks, and unsubscribes.
+
+        :param list_id: The unique id for the list.
+        :type list_id: :py:class:`str`
+        :param subscriber_hash: The MD5 hash of the lowercase version of the
+          list member’s email address.
+        :type subscriber_hash: :py:class:`str`
+        :param queryparams: The query string parameters
+        queryparams['fields'] = []
+        queryparams['exclude_fields'] = []
+        """
+        subscriber_hash = check_subscriber_hash(subscriber_hash)
+        self.list_id = list_id
+        self.subscriber_hash = subscriber_hash
+        return self._mc_client._get(url=self._build_path(list_id, 'members', subscriber_hash, 'activity-feed'), **queryparams)
+
